da19b75025369944c2f096e61e8e98b669e9a2ff
Jane <jane-cargill@hotmail.co.uk>
2017-02-20 15:54:27 +0000

    homework shows api complete

---
+++ db/seeds.rb
@@ -0,0 +1,41 @@
+Show.delete_all()
+
+Show.create({
+  title: 'Friends',
+  series: 10,
+  description: 'Follows the personal and professional lives of six 20 to 30-something-year-old friends living in Manhattan.',
+  image: 'http://www.proprofs.com/quiz-school/topic_images/p191fmmvor1oji1gln3mr1526175a3.jpg',
+  programmeID: '1'
+  })
+
+Show.create({
+  title: 'The Big Bang Theory',
+  series: 10,
+  description: 'A woman who moves into an apartment across the hall from two brilliant but socially awkward physicists shows them how little they know about life outside of the laboratory.',
+  image: 'http://media.comicbook.com/2016/02/bigbangtheoryday-170934.png',
+  programmeID: '2'
+  })
+
+Show.create({
+  title: 'Grey\'s Anatomy',
+  series: 13,
+  description: 'Grey\'s Anatomy is a medical drama about a group of surgeons working at Seattle Grace Hospital.',
+  image: 'http://cdn.hercampus.com/s3fs-public/2016/01/17/GreysCoverPhoto.jpg',
+  programmeID: '3'
+  })
+
+Show.create({
+  title: 'House',
+  series: 8,
+  description: 'House (aka House MD), from executive producers Paul Attanasio, Katie Jacobs, David Shore, and Bryan Singer is a new take on mystery, where the villain is a medical malady and the hero is an irreverent, controversial doctor who trusts no one, least of all his patients.',
+  image: 'https://petersontoscano.files.wordpress.com/2010/05/w3elljioms.jpg',
+  programmeID: '4'
+  })
+
+Show.create({
+  title: 'Gossip Girl',
+  series: 6,
+  description: 'Privileged teens living on the Upper East Side of New York can hide no secret from the ruthless blogger who is always watching.',
+  image: 'https://i.ytimg.com/vi/vxi6NlFoj-o/maxresdefault.jpg',
+  programmeID: '5'
+  })
